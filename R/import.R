cd1e441dd4ead4a01c2e9133d31d8b704e0471d9
Johnson <euler96@gmail.com>
2016-11-12 14:44:17 +0800

    Move `gridExtra`, `ggthemes`to suggests packages

--- R/import.R
+++ R/import.R
@@ -2,10 +2,8 @@
 #' @import ggplot2
 #' @import graphics
 #' @import ade4
-#' @import ggthemes
 #' @importFrom grDevices adjustcolor hcl
 #' @importFrom iNEXT DataInfo as.abucount as.incfreq
 #' @importFrom Rcpp cppFunction
-#' @importFrom gridExtra grid.arrange
 NULL
 
